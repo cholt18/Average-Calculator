0dcd3346951e2564f09ec1b1fc683bce2e4ed708
cholt18 <56280005+cholt18@users.noreply.github.com>
2019-10-07 19:02:17 -0400

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main(){
+
+  std::cout << "Please enter 5 integers";;
+  int x = 0;
+  int sum = 0;
+  int num = 0;
+  while(cin >> x){
+    sum+= x;
+    num++;
+  }
+  float average = sum/num;
+  std::cout << "The average of the numbers is " << average << std::endl;
+  return 0;
+}
